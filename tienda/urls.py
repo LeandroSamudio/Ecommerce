9fc8cf9c00649168cd5473d5845bddbfe0954573
franhidalgocavs <61721723+franhidalgocavs@users.noreply.github.com>
2022-10-28 20:24:15 -0300

    Django Intermedio Modulo1 Unidad1 video10, ultimo commit de la unidad1

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,6 +4,6 @@ from tienda.views import VerImagenes
 
 urlpatterns = [
     path('cargar/', views.cargar_imagen, name="cargar"),
-    # path('<int:producto_id>/ver/', views.ver_imagen, name="ver"),
+    path('<int:producto_id>/ver/', views.ver_imagen, name="ver"),
     path('verimagenes/', VerImagenes.as_view(), name="verimagenes"),
 ]
