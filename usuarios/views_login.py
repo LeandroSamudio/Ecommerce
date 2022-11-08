6937eff83922301e262be558423fbe96a3ba7638
franhidalgocavs <61721723+franhidalgocavs@users.noreply.github.com>
2022-11-07 21:07:11 -0300

    Django intermedio modulo1 unidad3 video4

--- usuarios/views_login.py
+++ usuarios/views_login.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 def pagina_login(request):
     params={}
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("verimagenes")
+
+        else:
+            return render(request, "Usuarios/login.html", params)
     return render(request, "usuarios/login.html", params)
+
+
